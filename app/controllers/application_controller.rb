22a095a691b0976d148a22014a389ea981e9b283
Prabhu Vignesh <pabhuvignesh.k@hcl.com>
2016-10-18 17:28:49 +0530

    Second time

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # this is the change for git workflow
+  # i am changing it from prabhu's machine
   protect_from_forgery with: :exception
 end

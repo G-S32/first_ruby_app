ebe899eb960c9cbabc22afa452de845ee8d403c2
Gurjeet Singh <s.gurjeet32@gmail.com>
2016-10-18 17:23:15 +0530

    Update application_controller.rb
    to test git workflow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  # this is the change for git workflow
   protect_from_forgery with: :exception
 end
